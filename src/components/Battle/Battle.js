da8c489bc4d5cf59b877bf19ab9c483edd4af6a4
Dustin Walker <dustinleewalker123@gmail.com>
2025-04-10 13:26:09 -0500

    battle comp styling

--- src/components/Battle/Battle.js
+++ src/components/Battle/Battle.js
@@ -1,11 +1,19 @@
-// import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { movieService } from '../../services/movieService';
 import './Battle.scss';
 
-const Battle = async () => {
+const Battle = () => {
+  const [battlePair, setBattlePair] = useState(null);
 
-  const battlePair = await movieService.getMovieBattlePair();
-  console.log("Battle pair in the battle component:", battlePair);
+  useEffect(() => {
+    const fetchBattlePair = async () => {
+      const pair = await movieService.getMovieBattlePair();
+      console.log("Battle pair in the battle component:", pair);
+      setBattlePair(pair);
+    };
+
+    fetchBattlePair();
+  }, []);
 
   return (
     <div className="battle">
@@ -14,12 +22,12 @@ const Battle = async () => {
         <div className="battle__movie">
           <div className="movie__placeholder">
             <div className="movie__poster-placeholder">
-              <span>Select Movie</span>
+              <img src={battlePair?.movie_a?.poster_url} alt={battlePair?.movie_a?.title} />
             </div>
             <div className="movie__details">
               <div className="movie__info">
-                <h3>Title</h3>
-                <span className="movie__year">(Year)</span>
+                <h3>{battlePair?.movie_a?.title}</h3>
+                <span className="movie__year">{battlePair?.movie_a?.year}</span>
               </div>
             </div>
           </div>
@@ -28,12 +36,13 @@ const Battle = async () => {
         <div className="battle__movie">
           <div className="movie__placeholder">
             <div className="movie__poster-placeholder">
-              <span>Select Movie</span>
+            <img src={battlePair?.movie_b?.poster_url} alt={battlePair?.movie_b?.title} />
+
             </div>
             <div className="movie__details">
               <div className="movie__info">
-                <h3>Title</h3>
-                <span className="movie__year">(Year)</span>
+                <h3>{battlePair?.movie_b?.title}</h3>
+                <span className="movie__year">{battlePair?.movie_b?.year}</span>
               </div>
             </div>
           </div>
