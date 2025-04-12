e7e918f54fbd2875143b2f5c7c2f50ec3f303e5b
Dustin Walker <dustinleewalker123@gmail.com>
2025-04-12 11:13:43 -0500

    token issue

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Navbar from './components/Navbar/Navbar';
 import Sidebar from './components/Sidebar/Sidebar';
 import Battle from './components/Battle/Battle';
 import Footer from './components/Footer/Footer';
 import Landing from './pages/Landing/Landing';
+import { login } from './store/authSlice';
 import './styles/App.scss';
 
 function App() {
   const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      dispatch(login(null));
+    }
+  }, [dispatch]);
 
   console.log("Are we changing?",isAuthenticated);
   return (
